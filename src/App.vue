6fb1e30af579536f2928ee81d4b4edf577041aa6
kiboma <omayioben@gmail.com>
2024-07-28 00:00:05 +0300

    Use options API

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Ben Kiboma",
+      age: 25,
+      status: "active",
+      tasks: ['wash utensils', 'Read nove', 'Go shopping', 'Go skating']
+    }
+  }
+}
+
+</script>
+
+<template>
+<h1>Hello {{name}}. You are {{age}} years old and your status is {{status}}</h1>
+<p>Below are the tasks that you need to complete:</p>
+<ul v-for="task in tasks" v-bind:key="task">
+  <li>{{task}}</li>
+</ul>
+
+
+</template>
